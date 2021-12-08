54c63e184c16b5498519e13778628572f89ccf9f
root <juliwang>
2021-12-08 16:12:48 +0800

    [firecracker] Add some scripts for experoment

---
+++ run2poweroff.sh
@@ -0,0 +1,5 @@
+curl --unix-socket /tmp/firecracker.socket -i \
+    -X PUT "http://localhost/actions" \
+    -H  "accept: application/json" \
+    -H  "Content-Type: application/json" \
+    -d '{"action_type": "SendCtrlAltDel"}'
